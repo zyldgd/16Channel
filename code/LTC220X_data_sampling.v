7a2486b3061fafd0a034d400bf60b7379faf3401
zyldgd <1141822522@qq.com>
2018-08-25 14:19:31 +0800

    update ADC

--- code/LTC220X_data_sampling.v
+++ code/LTC220X_data_sampling.v
@@ -6,9 +6,22 @@
  */
 
 module LTC220X_data_sampling(
-input   
-
+input                 clk,
+input                 reset_n,
+input  reg   [15:0]   DATA,
+input                 CLKOUT_n, 
+input                 CLKOUT_p 
 );
+
+reg   [15:0]      data_reg = 0;
+
+
+always @(posedge CLKOUT_n ) begin
+    data_reg <= DATA;
+end
+
+
+
 
 endmodule 
 //end LTC220X_data_sampling
